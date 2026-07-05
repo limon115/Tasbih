681141c03f0abea42ca7e216d83a405fc82f5fa9
limon115 <khalid.hasan.limon2006@gmail.com>
2026-07-05 19:18:22 +0600

    fix: update Theme extension return types to ThemeData to resolve Flutter build crash

--- lib/utils/extentions/extentions.dart
+++ lib/utils/extentions/extentions.dart
@@ -6,8 +6,8 @@ extension ThemeExtensions on BuildContext {
   ThemeData get theme => Theme.of(this);
   IconThemeData get iconTheme => Theme.of(this).iconTheme;
   TextTheme get textTheme => Theme.of(this).textTheme;
-  AppBarTheme get appBarTheme => Theme.of(this).appBarTheme;
-  InputDecorationTheme get inputDecoration =>
+  AppBarThemeData get appBarTheme => Theme.of(this).appBarTheme;
+  InputDecorationThemeData get inputDecoration =>
       Theme.of(this).inputDecorationTheme;
   CheckboxThemeData get checkboxTheme => Theme.of(this).checkboxTheme;
   ElevatedButtonThemeData get elevatedButtonTheme =>
